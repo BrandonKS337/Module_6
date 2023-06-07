6eb5a90a4f09fb570edb3280e7d2959db221c17b
Project V2 <brandonks337@gmail.com>
2023-06-06 20:05:42 -0500

    commiting everything from yesterday and today

---
+++ Exercise1/exercise_1/src/Components/Greeting.jsx
@@ -0,0 +1,13 @@
+function Greeting(props) {
+    const { greetingName } = props
+
+    console.log(greetingName)
+
+    return (
+        <>
+        <h3>"Hello World"</h3>
+        </>
+    )
+}
+
+export default Greeting
