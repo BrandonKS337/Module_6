64805a59f839719aa7e91080582464a1b6e3cc55
Your Name <brandonks337@gmail.com>
2023-06-08 13:33:30 -0500

    first push with exercise_2 folder setup. vite installed locally.

---
+++ exercise_2/components/BigCats.jsx
@@ -0,0 +1,14 @@
+function BigCats() {
+  return [
+    { name: "Cheetah", latinName: "Acinonyx jubatus" },
+    { name: "Cougar", latinName: "Puma concolor" },
+    { name: "Jaguar", latinName: "Panthera onca" },
+    { name: "Leopard", latinName: "Panthera pardus" },
+    { name: "Lion", latinName: "Panthera leo" },
+    { name: "Snow leopard", latinName: "Panthera uncia" },
+    { name: "Tiger", latinName: "Panthera tigris" },
+  ];
+}
+
+
+export default BigCats
